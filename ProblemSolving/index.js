75519c767fb31a19a6d9fd84371cdadd9e7bb45f
RittikaDev <rittika.dev@gmail.com>
2025-06-02 22:48:24 +0600

    first commit

---
+++ ProblemSolving/index.js
@@ -0,0 +1,41 @@
+"use strict";
+/* TASK 1: ARRAY FILTERING AND MAPPING
+-CREATE AN ARRAY OF OBJECTS, EACH REPRESENTING A PERSON WITH PROPERTIES LIKE NAME, AGE, AND GENDER. WRITE A FUNCTION TO FILTER OUT ALL FEMALES AND THEN MAP THE REMAINING PEOPLE TO AN ARRAY OF NAMES. PRINT THE FINAL RESULT.
+*/
+const people = [
+    { name: "Monica Geller", age: 28, gender: "female" },
+    { name: "John Smith", age: 30, gender: "male" },
+    { name: "Mili Brown", age: 22, gender: "female" },
+    { name: "Mark Will", age: 35, gender: "male" },
+];
+const getMaleNames = (persons) => persons.filter((p) => p.gender === "male").map((p) => p.name);
+const books = [
+    { title: "Norwegian Wood", author: "Haruki Murakami", year: 1987 },
+    { title: "Chuti", author: "Rabindranath Thakur", year: 1892 },
+    { title: "Gone Girl", author: "Gillian Flynn", year: 2012 },
+];
+const getBookTitles = (bookArray) => bookArray.map((book) => book.title);
+// console.log("Book Titles Only:", getBookTitles(books));
+/**
+ * TASK 3: FUNCTION COMPOSITION
+-WRITE THREE FUNCTIONS: ONE TO SQUARE A NUMBER, ONE TO DOUBLE A NUMBER, AND ONE TO ADD 5 TO A NUMBER. COMPOSE THESE FUNCTIONS TO CREATE A NEW FUNCTION THAT SQUARES A NUMBER, DOUBLES THE RESULT, AND THEN ADDS 5.
+ */
+const square = (n) => n * n;
+const double = (n) => n * 2;
+const addFive = (n) => n + 5;
+const combinedFunction = (n) => addFive(double(square(n)));
+const cars = [
+    { make: "Volvo", model: "XC70", year: 2025 },
+    { make: "Volkswagen", model: "Tharu", year: 2018 },
+    { make: "Toyota", model: "GR Corolla", year: 2022 },
+];
+const sortCarsByYear = (carArray) => carArray.sort((a, b) => a.year - b.year);
+// console.log("Sorted Array:", sortCarsByYear(cars));
+/*
+TASK 5: FIND AND MODIFY
+-WRITE A FUNCTION THAT SEARCHES AN ARRAY OF OBJECTS FOR A SPECIFIC PERSON BY NAME. IF FOUND, MODIFY THEIR AGE PROPERTY. PRINT THE UPDATED ARRAY.
+*/
+const updatePersonAge = (arr, name, newAge) => {
+    return arr.map((person) => person.name === name ? Object.assign(Object.assign({}, person), { age: newAge }) : person);
+};
+console.log("Updated People:", updatePersonAge(people, "John Smith", 26));
